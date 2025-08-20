c8afe3da86e6a2b950415fcc73b7fe80f7cba131
AiswaryaRJ <aiswaryarosejacob@gmail.com>
2025-08-20 14:40:28 +0530

    Update stack.c

--- stack.c
+++ stack.c
@@ -1,84 +1,64 @@
 #include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#define MAX_SIZE 100
-int stack[MAX_SIZE];
-int top = -1;
-void Push(int element)
-{
-if (top == MAX_SIZE -1)
-{
-	printf("stack overflow ");
-	return;
+#define MAX 20
+int stack[MAX];
+int top=-1;
+
+int isFull(){
+	return top== MAX-1;
 }
-top++;
-stack[top] = element;
-}
-int Pop() {
-	if (top == -1) {
-	 	printf("stack underflow");
-	 	return -1;
-	 }
-int element = stack[top];
-top--;
-return element;
-}
-int Peek() {
- if (top == -1) {
-     printf("Stack is empty Underflow!\n");
-     return -1; 
- }
- return stack[top];
-}
-bool IsEmpty() {
- return (top == -1);
-}
-void Display()
-{ int i;
-if(IsEmpty())
- printf("stack is empty");
- else
- {
- printf("stack content are");
- for(i=top;i>=0;i--)
- printf("%d ",stack[i]);
- }}
+int isEmpty(){
+	return top==-1;}
+void push(int value){
+if(isFull()){
+	printf("stack overflow\n");}
+else{
+	stack[++top]=value;
+	printf("%d pushed to stack\n",value);}}
+int pop(){
+if(isEmpty()){
+	printf("stack underflow\n");
+	return -1;}
+else{
+	return stack[top--];}}
+int peek(){
+if(isEmpty()){
+	printf("stack empty\n");
+	return -1;}
+else{
+return stack[top];}}
+
 int main()
 {
- int item, choice;
-  printf("\t Implementation of Stack");
-  do{
-  printf("\n\n1.Push\n2.Pop\n3.Peek\n4.Display\n5.exit\n");
-  printf("enter your choice ");
-  scanf("%d", &choice);
- switch (choice) {
+int choice,item,n,i,j,m;
+printf("enter no of actions to be implemented\n");
+scanf("%d",&m);
+for(j=0;j<m;j++){
+printf("select a no 1:push 2:pop 3:peek 4:checkifempty 5:checkiffull \n");
+scanf("%d",&choice);
+switch(choice){
  case 1:
-  	printf("\nEnter The item to be pushed :: ");
-     scanf("%d", &item);
-     Push(item);
-     break;
-case 2:
-     if (IsEmpty())
-     printf("\nEmpty stack!Underflow !!");
-     else {
-     item = Pop();
-     printf("\nThe popped element is %d", item);
-     }
-     break;
-case 3:
-     if (IsEmpty())
-     printf("\nEmpty stack!Underflow !!");
-     else {
-     item = Peek();
-     printf("\nThe Top element is %d", item);
-     }
-     break;
-case 4:
-     Display();
-     break;
-case 5:
-     exit(0);
-    }
-} while (1);
-return 0;
-}
+  printf("enter no of element to be pushed\n");
+  scanf("%d",&n);
+  for(i=0;i<n;i++){
+  printf("enter elemnt to be pushed\n");
+  scanf("%d",&item);
+  push(item);}
+  break;
+ case 2:
+  pop();
+  printf("the popped element is %d \n",item);
+  break;
+ case 3:
+  peek();
+  printf("the element is %d \n",item);
+  break;
+ case 4:
+  isEmpty();
+  printf("the stack is empty %d \n",item);
+  break;
+ case 5:
+  isFull();
+  printf("the stack is full %d \n",item);
+  break;
+  }
+  }}
